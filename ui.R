b397deb686031ed128319887346fff22d990f3a6
Nishant Sirohi <63949867+sirohinishant@users.noreply.github.com>
2020-11-06 22:13:58 +0530

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,45 @@
+library(shiny)
+
+shinyUI(
+  navbarPage("Shiny Application Project",
+             tabPanel("Analysis",
+                      fluidPage(
+                        titlePanel("The relationship between variables and miles per gallon (MPG)"),
+                        sidebarLayout(
+                          sidebarPanel(
+                            selectInput("variable", "Variable:",
+                                        c("Number of cylinders" = "cyl",
+                                          "Displacement (cu.in.)" = "disp",
+                                          "Gross horsepower" = "hp",
+                                          "Rear axle ratio" = "drat",
+                                          "Weight (lb/1000)" = "wt",
+                                          "1/4 mile time" = "qsec",
+                                          "V/S" = "vs",
+                                          "Transmission" = "am",
+                                          "Number of forward gears" = "gear",
+                                          "Number of carburetors" = "carb"
+                                        )),
+                            
+                            checkboxInput("outliers", "Show BoxPlot's outliers", FALSE)
+                          ),
+                          
+                          mainPanel(
+                            h3(textOutput("caption")),
+                            
+                            tabsetPanel(type = "tabs", 
+                                        tabPanel("BoxPlot", plotOutput("mpgBoxPlot")),
+                                        tabPanel("Regression model", 
+                                                 plotOutput("mpgPlot"),
+                                                 verbatimTextOutput("fit")
+                                        )
+                            )
+                          )
+                        )
+                      )
+             ),
+             tabPanel("Github repository",
+                      a("https://github.com/sirohinishant/DevelopingDataProducts-PeerGraded"),
+                      hr(),
+             )
+  )
+)
